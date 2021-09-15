7bedfe3e725e91175f8999023f79e6d8b880ce75
Heberto <heberto@maniak.com.mx>
2021-09-15 18:10:38 -0500

    add state to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Results from "./components/Results";
 import CountriesDropdown from "./components/CountriesDropdown";
+import SalaryInput from "./components/SalaryInput"
 import "./assets/styles/App.scss";
 import taxRates from "./taxrates.json";
 
@@ -10,11 +11,49 @@ const makeCurrencyList = (countries: object): string[] => {
 
 const makeCountryList = (taxRates: object): string[] => Object.keys(taxRates);
 
+interface appState {
+  selectedCountry?: string;
+  countryTaxRate: 0;
+  countryCurrency?: undefined;
+  annualSalary: number;
+  localTaxes: number;
+  approxMonthlyPayroll: number;
+}
+
 const App = (): JSX.Element => {
+  const [state, setState] = useState<appState>({
+    selectedCountry: undefined,
+    countryTaxRate: 0,
+    countryCurrency: undefined,
+    annualSalary: 0,
+    localTaxes: 0,
+    approxMonthlyPayroll: 0,
+  });
+
+  const onChangeCountry = (newCountry: { value: string; label: string }) => {
+    const countryName = newCountry.value;
+    setState((prevState) => ({
+      ...prevState,
+      selectedCountry: countryName,
+      countryTaxRate: taxRates[countryName].tax_rate,
+      countryCurrency: taxRates[countryName].currency,
+    }));
+  };
+
+  const onChangeSalary = (newSalary: string)
+
+  const calculateResult = () => {};
+
   return (
     <div className="App">
       <h1>Employer Tax Calculator</h1>
-      <CountriesDropdown countries={makeCountryList(taxRates)} />
+      <div className="first-form">
+        <CountriesDropdown
+          countries={makeCountryList(taxRates)}
+          onSelect={onChangeCountry}
+        />
+        <SalaryInput />
+      </div>
       <Results />
     </div>
   );
