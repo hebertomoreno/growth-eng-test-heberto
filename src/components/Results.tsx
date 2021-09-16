b3eb85afbc295c7cb45c523a5d020c7767c03866
Heberto <heberto@maniak.com.mx>
2021-09-15 19:18:54 -0500

    Show Results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,26 @@
 import React from "react";
 
-const Results = (): JSX.Element => <div className="results">Results</div>;
+interface resultsProps {
+  annualSalary: string;
+  localTaxes: number;
+  totalAnnualCost: number;
+  approxMonthlyPayroll: number;
+}
+
+const Results = (props: resultsProps): JSX.Element => {
+  // TODO: Style this Component.
+  // TODO: move interface to interface file
+  // TODO: show results on USD
+  const { annualSalary, localTaxes, totalAnnualCost, approxMonthlyPayroll } =
+    props;
+  return (
+    <div className="results">
+      <div>Annual Salary: {annualSalary}</div>
+      <div>Local Taxes: {localTaxes.toFixed(2)}</div>
+      <div>Total Annual Cost: {totalAnnualCost.toFixed(2)}</div>
+      <div>Approx. Monthly Payroll: {approxMonthlyPayroll.toFixed(2)}</div>
+    </div>
+  );
+};
+
 export default Results;
