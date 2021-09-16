f88df0d3a4410ccfdef29befccffaa043e116447
Heberto <heberto@maniak.com.mx>
2021-09-15 19:19:49 -0500

    add functions for changing input and calculating results

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import Results from "./components/Results";
 import CountriesDropdown from "./components/CountriesDropdown";
-import SalaryInput from "./components/SalaryInput"
+import SalaryInput from "./components/SalaryInput";
 import "./assets/styles/App.scss";
 import taxRates from "./taxrates.json";
+import { appStateInterface } from "./helpers/interfaces";
 
 const makeCurrencyList = (countries: object): string[] => {
   return Object.values(countries).map((country) => country.currency);
@@ -11,24 +12,20 @@ const makeCurrencyList = (countries: object): string[] => {
 
 const makeCountryList = (taxRates: object): string[] => Object.keys(taxRates);
 
-interface appState {
-  selectedCountry?: string;
-  countryTaxRate: 0;
-  countryCurrency?: undefined;
-  annualSalary: number;
-  localTaxes: number;
-  approxMonthlyPayroll: number;
-}
-
 const App = (): JSX.Element => {
-  const [state, setState] = useState<appState>({
+  const [state, setState] = useState<appStateInterface>({
     selectedCountry: undefined,
     countryTaxRate: 0,
     countryCurrency: undefined,
-    annualSalary: 0,
+    annualSalary: "",
     localTaxes: 0,
+    totalAnnualCost: 0,
     approxMonthlyPayroll: 0,
+    showResult: false,
   });
+
+  //TODO: Make app responsive
+  //TODO: Style the whole app
 
   const onChangeCountry = (newCountry: { value: string; label: string }) => {
     const countryName = newCountry.value;
@@ -40,9 +37,29 @@ const App = (): JSX.Element => {
     }));
   };
 
-  const onChangeSalary = (newSalary: string)
+  const onChangeSalary = (e: any) => {
+    const newSalary = e.target.value;
+    setState((prevState) => ({
+      ...prevState,
+      annualSalary: newSalary,
+    }));
+  };
 
-  const calculateResult = () => {};
+  const calculateResult = (e: any) => {
+    const currentSalary = parseFloat(state.annualSalary);
+    const localTaxes = currentSalary * state.countryTaxRate;
+    const totalAnnualCost = currentSalary + localTaxes;
+    const approxMonthlyPayroll = totalAnnualCost / 12;
+
+    setState((prevState) => ({
+      ...prevState,
+      localTaxes: localTaxes,
+      totalAnnualCost: totalAnnualCost,
+      approxMonthlyPayroll: approxMonthlyPayroll,
+      showResult: !prevState.showResult,
+    }));
+    e.preventDefault();
+  };
 
   return (
     <div className="App">
@@ -52,9 +69,18 @@ const App = (): JSX.Element => {
           countries={makeCountryList(taxRates)}
           onSelect={onChangeCountry}
         />
-        <SalaryInput />
+        <SalaryInput
+          salary={state.annualSalary}
+          handleChange={onChangeSalary}
+          handleSubmit={calculateResult}
+        />
       </div>
-      <Results />
+      <Results
+        annualSalary={state.annualSalary}
+        localTaxes={state.localTaxes}
+        totalAnnualCost={state.totalAnnualCost}
+        approxMonthlyPayroll={state.approxMonthlyPayroll}
+      />
     </div>
   );
 };
